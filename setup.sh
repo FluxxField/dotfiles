6fb7b1e627d85aa56e9cdb9c9bd47042643c9694
Keenan <keenanjj13@gmail.com>
2022-02-23 19:40:15 -0600

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -17,9 +17,29 @@ echo ""
 brew install wget
 brew install gnupg
 brew install grep
+brew install node
+brew install yarn
+
+echo "installing asdf"
+echo ""
+brew install asdf
+echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add yarn
+asdf install nodejs latest
+asdf install yarn latest
+asdf global nodejs latest
+asdf global yarn latest
 
 echo "installing vim and nvim"
+echo ""
 brew install vim
 brew install neovim
 
 cp -R /.config/nvim/init.vim ~/.config/nvim/
+
+echo "install oh-my-zsh"
+echo ""
+curl -fsSL https://raw.github.com/ohmyzsh/master/tools/install.sh
+git clone https://github.com/spaceship-promp/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
