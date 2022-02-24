0b50af0e3e859f8c3478f5ff1c60f61dbfc10b4b
Keenan <keenanjj13@gmail.com>
2022-02-23 22:36:41 -0600

    updated setup.sh with if checks

--- setup.sh
+++ setup.sh
@@ -1,49 +1,82 @@
 #!/usr/bin/env zsh
 
-echo "installing brew"
-echo ""
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh;
+which -s brew
+if [[ $? != 0 ]]; then
+  echo "##############################"
+  echo "# installing brew"
+  echo "##############################"
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh;
 
-echo "updating brew"
-echo ""
-brew update
+  echo "##############################"
+  echo "# installing packages"
+  echo "##############################"
+  brew install wget
+  brew install gnupg
+  brew install grep
+  brew install node
+  brew install yarn
+  brew install tmux
+else
+  echo "##############################"
+  echo "# updating brew"
+  echo "##############################"
+  brew update
+fi
 
-echo "upgrading brew"
-echo ""
+echo "##############################"
+echo "# upgrading brew"
+echo "##############################"
 brew upgrade
 
-echo "installing packages"
-echo ""
-brew install wget
-brew install gnupg
-brew install grep
-brew install node
-brew install yarn
-brew install tmux
+which -s asdf
+if [[ $? != 0 ]]; then
+  echo "##############################"
+  echo "# installing asdf"
+  echo "##############################"
+  brew install asdf
+  echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrcw
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf plugin-add yarn
+  asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+  asdf install nodejs latest
+  asdf install yarn latest
+  asdf install golang latest
+  asdf global nodejs latest
+  asdf global yarn latest
+  asdf global golang latest
+fi
 
-echo "installing asdf"
-echo ""
-brew install asdf
-echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add yarn
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf install nodejs latest
-asdf install yarn latest
-asdf install golang latest
-asdf global nodejs latest
-asdf global yarn latest
-asdf global golang latest
+which -s vim
+if [[ $? != 0 ]]; then
+  echo "##############################"
+  echo "# installing vim"
+  echo "##############################"
+  brew install vim
+fi
 
-echo "installing vim and nvim"
-echo ""
-brew install vim
-brew install neovim
+which -s nvim
+if [[ $? != 0 ]]; then
+  echo "##############################"
+  echo "# installing neovim"
+  echo "##############################"
+  brew install neovim
+  # overridding init.vim with repo version (points to .vimrc)
+  cp /.config/nvim/init.vim ~/.config/nvim/
+fi
 
-cp /.config/nvim/init.vim ~/.config/nvim/
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "##############################"
+  echo "# installing oh-my-zsh"
+  echo "##############################"
+  # Install oh-my-zsh
+  curl -fsSL https://raw.github.com/ohmyzsh/master/tools/install.sh
+  # install spaceship theme for oh-my-zsh
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+  # symlink theem
+  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+  # install autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  # install syntax highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
-echo "install oh-my-zsh"
-echo ""
-curl -fsSL https://raw.github.com/ohmyzsh/master/tools/install.sh
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
