1dd7fafb5e1ec428386d0a4b75c6a094a0942100
Keenan <keenanjj13@gmail.com>
2022-02-24 11:55:49 -0600

    more testing

--- extract.sh
+++ extract.sh
@@ -9,13 +9,15 @@ function extract() {
   source ~/.zshrc
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" || "$1" == "-f" ]]
+then
   extract
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo ""
 
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
     extract
   fi
 fi
