2a008d5aac57ea7907218142f85fc48cab12dfed
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2022-03-02 10:04:13 -0600

    Update post_install.sh

--- .config/zsh/post_install.sh
+++ .config/zsh/post_install.sh
@@ -1,2 +1,5 @@
-# Post installations for
-[[ "${VAR_BREW_BINS[@]} " =~ " neovim "]] && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && nvim +PlugInstall +qall
+# Post-Installation for 
+[[ "${VAR_BREW_BINS[@]} " =~ " neovim " ]] && \
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs  \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && \
+    nvim +PlugInstall +qall
