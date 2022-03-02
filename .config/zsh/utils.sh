e8efb4d506e286a08c97c3aa709a2155180adee0
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2022-03-02 11:57:13 -0600

    install_nvim_req

--- .config/zsh/utils.sh
+++ .config/zsh/utils.sh
@@ -146,7 +146,11 @@ function install_ohmyzsh () {
   fi
 }
 
-function nvim_handlers () {
+function install_nvim_req () {
+    # install vim-plug
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       
   npm install -g neovim
   yarn global add neovim
   python3 -m pip install pynvim --user
