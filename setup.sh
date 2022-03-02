96aa78235699b40dc8ee3de171f32269caad2763
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2022-03-02 10:06:04 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
-# Loads the zsh vars. Checks the home dir then falls back to Github
-[ -f $HOME/.config/zsh/vars.sh ] && source $HOME/.config/zsh/vars.sh \ || source /dev/stdin <<< "$(curl https://raw.githubusercontent.com/FluxxField/dotfiles/main/.config/zsh/vars.sh)"
+# Load the variables in the current from the local vars.sh file if present under
+# config otherwise read from the fallback file at github.
+[ -f $HOME/.config/zsh/vars.sh  ] && source $HOME/.config/zsh/vars.sh  \
+                                  || source /dev/stdin <<< "$(curl https://raw.githubusercontent.com/kalkayan/dotfiles/main/.config/zsh/vars.sh)"
 
-# Loads the utility functions. Checks the home dir then falls back to Github
-[ -f $HOME/.config/zsh/utils.sh ] && source $HOME/.config/zsh/utils.sh \ || source /dev/stdin "$(curl https://raw.githubusercontent.com/FluxxField/dotfiles/main/.config/zsh/utils.sh)"
 
+# Load utility functions from the utils.sh file (where all the heavy lifting 
+# installation code is written) otherwise read them from the fallback file.
+[ -f $HOME/.config/zsh/utils.sh ] && source $HOME/.config/zsh/utils.sh \
+                                  || source /dev/stdin <<< "$(curl https://raw.githubusercontent.com/kalkayan/dotfiles/main/.config/zsh/utils.sh)"
+                                  
 ask_password
 
 install_xcode
@@ -18,12 +23,12 @@ install_bins $VAR_BREW_BINS
 
 install_casks $VAR_BREW_CASKS
 
+. $HOME/.config/zsh/post_install.sh
+
 install_ohmyzsh
 
 nvim_handlers
 
 asdf_setup
-
-. $HOME/.config/zsh/post_install.sh
 
 source $HOME.zshrc
