e1dc43904c3a883b11c898b3dfcc19e9aa3bef5b
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2022-03-02 10:01:07 -0600

    Update vars.sh

--- .config/zsh/vars.sh
+++ .config/zsh/vars.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 VAR_DOTFILES_BRANCH="main"
 
 VAR_BREW_BINS=( $(cut -d '=' -f1 $HOME/.config/brew/bins.txt) )
 VAR_BREW_CASKST=( $(cut -d '=' -f1 $HOME/.config/brew/casks.txt) )
+
+PRIMARY='\033[36m'
+SECONDARY='\033[33m'
+TERTIARY='\033[92m'
